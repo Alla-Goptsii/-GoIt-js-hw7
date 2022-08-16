410bdcacf6c559dce93d37f5780f5031c9ce57f7
Alla-Goptsii <goptsy.a@gmail.com>
2022-08-16 20:54:16 +0300

    correct alt(неудачно

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,63 +1,123 @@
+// import { galleryItems } from "./gallery-items.js";
+// // Change code below this line
+// console.log(galleryItems);
+
+// const currentContainer = document.querySelector(".gallery");
+// const cardsGallery = addItemCard(galleryItems);
+
+// currentContainer.insertAdjacentHTML("beforeend", cardsGallery);
+
+// function addItemCard(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+//     <div class="gallery__item">
+//       <a class="gallery__link" href="img.[data-source="${original}"]">
+//         <img
+//           class="gallery__image"
+//           src="${preview}"
+//           data-source="${original}"
+//           alt="${description}"
+//         />
+//       </a>
+//     </div>
+//     `;
+//     })
+//     .join("");
+// }
+
+// currentContainer.addEventListener("click", onClickCard);
+
+// const instance = basicLightbox.create(`
+//   <div class="content">
+//         <img
+//           src=""
+//           alt="full-image"
+//         />
+//     </div>
+//   `);
+
+// function onClickCard(evt) {
+//   evt.preventDefault();
+
+//   if (!evt.target.classList.contains("gallery__image")) {
+//     return;
+//   }
+
+//   let selectedImg = evt.target.dataset.source;
+//   const loadImage = instance.element().querySelector("img");
+//   loadImage.src = selectedImg;
+
+//   instance.show();
+//   window.addEventListener("keydown", onEscKeyPress);
+// }
+
+// function onEscKeyPress(event) {
+//   if (event.code === "Escape") {
+//     onCloseModal();
+//   }
+// }
+
+// function onCloseModal() {
+//   instance.close();
+//   window.removeEventListener("keydown", onEscKeyPress);
+// }
+
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-const currentContainer = document.querySelector(".gallery");
-const cardsGallery = addItemCard(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
 
-currentContainer.insertAdjacentHTML("beforeend", cardsGallery);
-
-function addItemCard(galleryItems) {
+function createImgGallery(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
     <div class="gallery__item">
-      <a class="gallery__link" href="img.[data-source="${original}"]">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
     </div>
     `;
     })
     .join("");
 }
 
-currentContainer.addEventListener("click", onClickCard);
+const imgMurkup = createImgGallery(galleryItems);
 
-const instance = basicLightbox.create(`
-  <div class="content">
-        <img
-          src=""
-          alt="full-image"
-        />
-    </div>
-  `);
+galleryContainer.insertAdjacentHTML("beforeend", imgMurkup);
 
-function onClickCard(evt) {
+galleryContainer.addEventListener("click", onGalleryImgClick);
+
+const instance = basicLightbox.create(`<img src=""/>`);
+
+function onGalleryImgClick(evt) {
   evt.preventDefault();
 
-  if (!evt.target.classList.contains("gallery__image")) {
+  const isImgSwatchEl = evt.target.classList.contains("gallery__image");
+  if (!isImgSwatchEl) {
     return;
   }
 
-  let selectedImg = evt.target.dataset.source;
-  const loadImage = instance.element().querySelector("img");
-  loadImage.src = selectedImg;
+  let selectedImage = evt.target.dataset.source;
 
+  const modalImage = instance.element().querySelector("img");
+  modalImage.src = selectedImage;
   instance.show();
-  window.addEventListener("keydown", onEscKeyPress);
-}
 
-function onEscKeyPress(event) {
-  if (event.code === "Escape") {
-    onCloseModal();
-  }
+  galleryContainer.addEventListener("keydown", onEscClick);
 }
 
 function onCloseModal() {
   instance.close();
-  window.removeEventListener("keydown", onEscKeyPress);
+  window.removeEventListener("keydown", onEscClick);
+}
+
+function onEscClick(evt) {
+  if (evt.code === "Escape") {
+    onCloseModal();
+  }
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,20 +1,44 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const currentContainer = document.querySelector(".gallery");
-const cardsGallery = addItemCard(galleryItems);
+// const currentContainer = document.querySelector(".gallery");
+// const cardsGallery = addItemCard(galleryItems);
 
-currentContainer.insertAdjacentHTML("beforeend", cardsGallery);
+// currentContainer.insertAdjacentHTML("beforeend", cardsGallery);
 
-function addItemCard(galleryItems) {
+// function addItemCard(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+//     <li>
+//     <a class="gallery__item" href="${original}">
+//   <img class="gallery__image" src="${preview}" alt="${description}" />
+// </a></li>
+//     `;
+//     })
+//     .join("");
+// }
+
+// new SimpleLightbox(".gallery a", {
+//   captionsData: "alt",
+//   captionsDelay: 250,
+// });
+
+const galleryContainer = document.querySelector(".gallery");
+const imgMurkup = createImgGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imgMurkup);
+
+function createImgGallery(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-    <li>
-    <a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a></li>
-    `;
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}"/>
+        </a>
+        `;
     })
     .join("");
 }
